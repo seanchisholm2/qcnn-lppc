6fc6f4a1bbc4ae3002c180598780e65b5ab5b197
seanchisholm2 <70863801+seanchisholm2@users.noreply.github.com>
2024-07-02 11:20:41 -0400

    Setup Python File

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup, find_packages
+
+
+# In[ ]:
+
+
+setup(
+    name="lppc_qcnn_package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pennylane",
+        "matplotlib",
+        "torch",
+        "math",
+        "random",
+        "csv",
+        "tqdm",
+        "scipy"
+    ],
+    description="PennyLane Quantum Convolutional Neural Network Package (LPPC)",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/seanchisholm2/qcnn-lppc",
+    author="Sean Chisholm",
+    author_email="schisholm@college.harvard.edu",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
